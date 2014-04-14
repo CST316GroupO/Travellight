b4b0dda8cad87820ec4a70bb221f2e4e2e7b6407
brandonespana <biespana@asu.edu>
2014-04-14 00:21:43 -0700

    Trip from file attachment is now added to trip hub drawer

--- app/src/main/java/groupo/travellight/app/TripActivity.java
+++ app/src/main/java/groupo/travellight/app/TripActivity.java
@@ -30,7 +30,7 @@ public class TripActivity extends ActionBarActivity implements NavigationDrawerF
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-
+    private ArrayAdapter mDrawerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +54,10 @@ public class TripActivity extends ActionBarActivity implements NavigationDrawerF
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
+        mDrawerAdapter = new ArrayAdapter<String>(this,R.layout.popup_layout,trips);//this line was blank
         // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.popup_layout, trips));
+        //mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.popup_layout, trips));
+        mDrawerList.setAdapter(mDrawerAdapter);//this line didn't exist
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
@@ -265,7 +266,24 @@ public class TripActivity extends ActionBarActivity implements NavigationDrawerF
 
 
     }
+//this line was blank and was immediately before the block comment and the onResume was not im plemented
+    @Override
+    public void onResume(){
+        super.onResume();
+        File folder = new File(getApplicationContext().getFilesDir().getPath().toString() + "/" + mEmail);
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++)
+        {
 
+            if (listOfFiles[i].isDirectory())
+            {
+                trips.add(listOfFiles[i].getName());
+
+            }
+        }
+
+        mDrawerAdapter.notifyDataSetChanged();
+    }
     /**
      * A placeholder fragment containing a simple view.
      */
