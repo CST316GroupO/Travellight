bba324887c898f50236c39e374f9f53ae93bafba
gmann1 <gmann1@asu.edu>
2014-04-23 08:46:51 -0700

    Utility got deleted somehow. readded it.

---
+++ app/src/main/java/groupo/travellight/app/Utility.java
@@ -0,0 +1,29 @@
+package groupo.travellight.app;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class Utility {
+    public static ArrayList<String> nameOfEvent = new ArrayList<String>();
+    public static ArrayList<String> startDates = new ArrayList<String>();
+    public static ArrayList<String> endDates = new ArrayList<String>();
+    public static ArrayList<String> descriptions = new ArrayList<String>();
+
+    public static ArrayList<String> readCalendarEvent(Context context) {
+
+
+        return nameOfEvent;
+    }
+
+    public static String getDate(long milliSeconds) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
+    }
+}
